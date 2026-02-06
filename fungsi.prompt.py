68663c9f812aaf3f98c26eae7af2bc95b44fdb58
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2026-02-07 01:19:21 +0700

    ...

---
+++ fungsi.prompt.py
@@ -0,0 +1,9 @@
+# fungsi tentang luas persegi panjang
+
+def menghitung_luas_persegi_panjang(p,l):
+    luasnya = p * l
+    return luasnya
+
+
+persegiyangpengen = menghitung_luas_persegi_panjang(10,199)
+print(persegiyangpengen)

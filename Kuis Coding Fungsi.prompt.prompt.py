78defd97b1dbedfc8741fd58b547f97ffc21b510
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2026-02-08 23:10:51 +0700

    cek in hari ini

---
+++ Kuis Coding Fungsi.prompt.prompt.py
@@ -0,0 +1,9 @@
+# Kuis Coding: Fungsi
+
+def minimal(a,b):
+    if a<= b:
+        return a
+    else:
+        return b
+
+print (minimal(2,33))
---
+++ atribut dan class oop.prompt.py
@@ -0,0 +1,15 @@
+# class dan atribut 
+class motor:
+    warna = "pink"
+
+motor1 = motor()
+print (motor.warna)
+
+motor2 = motor()
+print (motor2.warna)
+
+# ini ketika aku update 
+motor.warna = "hitam"
+
+print (motor1.warna)
+print (motor2.warna)

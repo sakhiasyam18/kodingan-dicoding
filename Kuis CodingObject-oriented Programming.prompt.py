06dc3e8a8ab3d4695e5e1301ca5da7ba864ceca6
sakhiasyam18 <asyamsakhimaulana@gmail.com>
2026-02-16 23:15:14 +0700

    .....

---
+++ Kuis CodingObject-oriented Programming.prompt.py
@@ -0,0 +1,16 @@
+# Kuis Coding: Object-oriented Programming
+
+class Animal:
+    def __init__(self, name, age, species):
+        self.name = name
+        self.age = age
+        self.species = species
+
+class Cat(Animal):
+    def deskripsi(self):
+        return f"{self.name} adalah kucing berjenis {self.species} yang sudah berumur {self.age} tahun"
+
+    def suara(self):
+        return "meow!"
+
+myCat = Cat("Neko", 3, "Persian")
